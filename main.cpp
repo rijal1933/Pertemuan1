091b33342b9fbe5e9d0ec27d74d4d2642464917b
rijal1933 <112966656+rijal1933@users.noreply.github.com>
2022-09-06 23:07:10 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama: Alrijal Nur Ilham" << endl;
+    cout << "Nim: A11.2022.14113" << endl;
+    cout << "Alamat: Tegalreja,Banjarharjo,Brebes" << endl;
+    cout << "Asal Sekolah: SMAN 1 Banjarharjo" << endl;
+    return 0;
+}
